e46852812227666a17c001315ab7fc5ac8913ede
Yaelcome <158767119+Yaelcome@users.noreply.github.com>
2024-05-10 21:14:19 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const input =  'turpentine and turtles';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [] 
+
+for (let i = 0; i < input.length; i++) {
+// console.log(input[i]);
+if (input[i] === 'e' || input[i] === 'u') {
+  resultArray.push(input[i])
+} 
+for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+    // console.log(`Letter ${vowels[j]} is a vowel`);
+     resultArray.push(input[i]);
+    }
+  }
+};
+
+//console.log(resultArray);
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString)
